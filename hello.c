927bac757853ec8631f919fc8bca31706ca3dd75
ChiragDAD01 <chiragdadhich0@gmail.com>
2026-03-25 10:15:10 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,a;
+    printf("your no. is %d",&a);
+    printf("CHIRAG DADHICH \nfirst.c \n file \n" );
+
+    
+    if(3==2){
+        printf("a greater than equals to 2");
+    }
+    else{
+        printf("a smaller than equals to 2");
+    }
+
+    for ( i = 9; i < 10; i++)
+    {
+        printf("\n kartik dost \n");
+    }
+    
+
+    return 0;
+}
